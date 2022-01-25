b50522ab1881d9a42d560258ee70d85b8adec34a
Anoop Lamba <anooplamba77@gmail.com>
2022-01-25 17:59:05 +0530

    Create ReturnArraySum.java

---
+++ ReturnArraySum.java
@@ -0,0 +1,20 @@
+/*
+Return array sum
+Question - Given an array/list(ARR) of length N, you need to find and return the sum of all the elements in the array/list
+*/
+
+public class Solution {
+	
+	public static int sum(int[] arr) {
+		int n = arr.length;
+        int sum = 0;
+        
+        int i = 0, j = n-1;
+        while(i <= j) {
+            sum += arr[i];
+            i++;
+        }
+        
+        return sum;
+	}
+}
